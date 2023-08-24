1ba7427f1f60bd6da02ff1648c94cbf101491c21
sud2701 <112654802+sud2701@users.noreply.github.com>
2023-08-24 09:48:18 -0700

    Time: 2 ms (10.93%), Space: 40.6 MB (97.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,18 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
+        // int n = nums.length;
+        // int min;
+        // for(int i = 0; i < n - 1; i++){
+        //     min = i;
+        //     for(int j = i + 1; j < n; j++){
+        //         if(nums[j] < nums[min]){
+        //             min = j;
+        //         }
+        //     }
+        //     if(min != i){
+        //         nums[i] = nums[i] + nums[min];
+        //         nums[min] = nums[i] - nums[min];
+        //         nums[i] = nums[i] - nums[min];
+        //     }
+        // }
+
         int n = nums.length;
-        int min;
-        for(int i = 0; i < n - 1; i++){
-            min = i;
-            for(int j = i + 1; j < n; j++){
-                if(nums[j] < nums[min]){
-                    min = j;
+        for(int i = 1; i < n; i++){
+            if(nums[i] < nums[i - 1]){
+                while(i > 0 && nums[i] < nums[i - 1]){
+                    nums[i] = nums[i] + nums[i - 1];
+                    nums[i - 1] = nums[i] - nums[i - 1];
+                    nums[i] = nums[i] - nums[i - 1];
+                    i -= 1;
                 }
-            }
-            if(min != i){
-                nums[i] = nums[i] + nums[min];
-                nums[min] = nums[i] - nums[min];
-                nums[i] = nums[i] - nums[min];
             }
         }
     }
