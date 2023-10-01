875e5a0d6935ae4580ecf246b9569cc8a158ea29
sud2701 <112654802+sud2701@users.noreply.github.com>
2023-09-30 21:59:42 -0700

    Time: 2 ms (57.36%), Space: 42.2 MB (24.63%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -0,0 +1,37 @@
+public class Codec {
+    HashMap<String, String> urlMap;
+    static int count = 1;
+    char[] codes = new char[] {'A', 'B', 'C', 'D', 'E', 'F'};
+    Codec() {
+        urlMap = new HashMap<String, String>();
+    }
+
+    public String hexCode(int num) {
+        StringBuilder hex = new StringBuilder();
+        while(num > 0){
+            if(num % 16 > 9){
+                hex.append(String.valueOf(codes[num % 16 - 10]));
+            }
+            else {
+                hex.append(String.valueOf(num % 16));
+            }
+            num = num / 16;
+        }
+        return hex.toString();
+    }
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String hashval = hexCode(Codec.count);
+        urlMap.put(hashval, longUrl);
+        return hashval;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return urlMap.get(shortUrl);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
