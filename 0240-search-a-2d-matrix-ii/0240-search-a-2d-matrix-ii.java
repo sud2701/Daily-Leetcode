39d66c849aabdd8e1783ed288a0b1478a1987979
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-06-01 12:21:33 -0700

    Time: 7 ms (23.06%), Space: 46.3 MB (5.9%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -64,7 +64,7 @@ class Solution {
 
         int m = matrix.length;
         int n = matrix[0].length;
-
+        int min_diagonal = 0;
         for(int i = 0; i < m; i++){
             if(matrix[i][0] == target || matrix[i][n - 1] == target){
                 return true;
