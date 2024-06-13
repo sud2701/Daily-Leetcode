1e42242706ab159291a31e088e20d05182373de8
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-06-13 15:08:49 -0700

    Time: 0 ms (100%), Space: 42.1 MB (8.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,31 +1,17 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // int n = nums.length;
-        // int min;
-        // for(int i = 0; i < n - 1; i++){
-        //     min = i;
-        //     for(int j = i + 1; j < n; j++){
-        //         if(nums[j] < nums[min]){
-        //             min = j;
-        //         }
-        //     }
-        //     if(min != i){
-        //         nums[i] = nums[i] + nums[min];
-        //         nums[min] = nums[i] - nums[min];
-        //         nums[i] = nums[i] - nums[min];
-        //     }
-        // }
-
+        int[] freqs = new int[3];
+        for(int i : nums) {
+            freqs[i] += 1;
+        }
         int n = nums.length;
-        for(int i = 1; i < n; i++){
-            if(nums[i] < nums[i - 1]){
-                while(i > 0 && nums[i] < nums[i - 1]){
-                    nums[i] = nums[i] + nums[i - 1];
-                    nums[i - 1] = nums[i] - nums[i - 1];
-                    nums[i] = nums[i] - nums[i - 1];
-                    i -= 1;
-                }
-            }
+        int curr_index = 0;
+        for(int i = 0; i < n; i++) {
+            while(freqs[curr_index] == 0){
+                curr_index++;
+            } 
+            nums[i] = curr_index;
+            freqs[curr_index]--;
         }
     }
 }
