dee96e754be30d8fde2d7e229de55add7ccc144f
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-07-08 16:21:01 -0700

    Time: 9 ms (42.66%), Space: 67.9 MB (19.02%) - LeetHub

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ArrayList<Pair> points = new ArrayList<Pair> ();
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int index = 1;
+        int len = 0;
+        int[] result = {-1, -1};
+        while(curr.next != null) {
+            if(prev.val < curr.val && curr.next.val < curr.val) {
+                points.add(new Pair(curr.val, index));
+                len++;
+            }
+            else if(prev.val > curr.val && curr.next.val > curr.val) {
+                points.add(new Pair(curr.val, index));
+                len++;
+            }
+            index += 1;
+            prev = curr;
+            curr = curr.next;
+        }
+        if(len >= 2) {
+            int min_dist = 1000000;
+            Pair x;
+            Pair y;
+            for(int i = 0; i < len - 1; i++) {
+                x = points.get(i);
+                y = points.get(i + 1);
+                min_dist = Math.min(min_dist, Math.abs(x.index - y.index));
+            }
+            result[0] = min_dist;
+            int start = 0;
+            int end = len - 1;
+            result[1] = points.get(end).index - points.get(start).index;
+        }
+        return result;
+    }
+}
+
+class Pair {
+    int value;
+    int index;
+
+    Pair(int value, int index) {
+        this.index = index;
+        this.value = value;
+    }
+}
