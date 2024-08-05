cd58c590c09bec6ab02e0856a5415f5206230d5b
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-08-05 10:53:51 -0700

    Time: 7 ms (26.65%), Space: 43.9 MB (93.32%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int temp = 0;
+        for(int i: target) {
+            if(map.get(i) == null) {
+                map.put(i, 1);
+            }
+            else {
+                temp = map.get(i);
+                map.put(i, temp + 1);
+            }
+        }
+        for(int i: arr) {
+            if(map.get(i) == null || map.get(i) == 0) {
+                return false;
+            }
+            else {
+                temp = map.get(i);
+                map.put(i, temp - 1);
+            }
+        }
+        return true;
+    }
+}
