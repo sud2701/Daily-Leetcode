57152477f34c8b9c2ffa118fc85007528593cd6b
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-08-05 10:39:33 -0700

    Time: 225 ms (7.38%), Space: 64.6 MB (33.2%) - LeetHub

---
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((a, b) -> a - b);
+        int total = 0;
+        int subtract_index = 0;
+        int temp = 0;
+        int curr_sub_num = 1;
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            total += nums[i];
+            queue.add(total);
+        }
+        while(subtract_index < n && !queue.isEmpty()) {
+            int elements = queue.size();
+            for(int i = 0; i < elements; i++) {
+                temp = queue.remove();
+                if(temp - nums[subtract_index] > 0){
+                    queue.add(temp - nums[subtract_index]);
+                }
+                minHeap.add(temp);
+            }
+            subtract_index++;
+        }
+        while(curr_sub_num <= right) {
+            temp = minHeap.poll();
+            if(curr_sub_num >= left) {
+                result = (result + temp) % 1_000_000_007;
+            }
+            curr_sub_num += 1;
+        }
+        return result;
+    }
+}
