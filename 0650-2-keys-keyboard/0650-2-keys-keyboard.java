d2a128869c99f6f83e87652dc3d2995c620dbc28
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-08-19 11:43:27 -0700

    Time: 37 ms (8.16%), Space: 41 MB (28.26%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -7,7 +7,7 @@ class Solution {
         dp[2] = 2;
         for(int i = 2; i <= n; i++){
             dp[i] = i;
-            for(int j = 2; j <= i / 2; j++){
+            for(int j = i / 2; j >= Math.sqrt(i); j--){
                 if(i % j == 0){
                     dp[i] = Math.min(dp[i], dp[j] + i / j);
                 }
