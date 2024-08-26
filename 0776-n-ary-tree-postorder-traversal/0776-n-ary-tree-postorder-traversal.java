4b9b6eff5a8a78f8f7d3b789b1deec59fdced4b9
Sudheer Reddy Kunduru <112654802+sud2701@users.noreply.github.com>
2024-08-26 12:13:40 -0700

    Time: 0 ms (100%), Space: 44.6 MB (50.84%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    ArrayList<Integer> result;
+    public List<Integer> postorder(Node root) {
+        result = new ArrayList<Integer>();
+        postOrderHelper(root);
+        return result;
+    }
+
+    public void postOrderHelper(Node root) {
+        if(root == null){
+            return;
+        }
+        else {
+            Iterator<Node> ib = root.children.iterator();
+            while(ib.hasNext()){
+                postOrderHelper(ib.next());
+            }
+            result.add(root.val);
+        }
+    }
+}
